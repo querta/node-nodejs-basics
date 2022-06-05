c7e10672f661ad02dc9f40b6fa6ef7c304f02e75
qurta <afy@dr.com>
2022-06-05 02:12:57 +0200

    init: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,25 @@
+import * as fs from 'fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        if (!(await checkExist('./files/fileToRead.txt')))
+            console.log(new Error("FS operation failed"));
+        else {
+            const file = await fs.readFile('./files/fileToRead.txt', 'utf-8');
+            console.log(file);
+        }
+    } catch(err) {
+        console.error(err);
+    }
 };
+read();
