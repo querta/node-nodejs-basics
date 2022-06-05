49a9562ae8190436dab9db5ce4ff05a865aab914
qurta <afy@dr.com>
2022-06-05 02:12:14 +0200

    init: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 export const rename = async () => {
-    // Write your code here 
+    try {
+        if (!(await checkExist('./files/wrongFilename.txt')) || await checkExist('./files/properFilename.md'))
+            console.log(new Error("FS operation failed"));    
+        else {
+            await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+        }
+    } catch(err) {
+        console.error(err);        
+    }
 };
+rename();
