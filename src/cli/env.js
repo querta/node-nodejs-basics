65fce9b3a72d3c3492b6a9216cdd523acab80300
qurta <afy@dr.com>
2022-06-05 03:07:41 +0200

    init: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    let result = "";
+    for (let key in env){
+        if (!key.indexOf('RSS_'))
+            result += `${key}=${env[key]}; `;
+    }
+    console.log(result.slice(0, -2));
 };
+
+parseEnv();
