99651386e594020f77a164972d396e1954630917
qurta <afy@dr.com>
2022-06-05 12:52:37 +0200

    refactor: rename stream to file in streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,8 +3,8 @@ import { open } from 'fs/promises';
 export const read = async () => {
     try {
         // const stream = await createReadStream('./files/fileToRead.txt', 'utf8');
-        const stream = await open('./files/fileToRead.txt');
-        stream.createReadStream().pipe(process.stdout);
+        const file = await open('./files/fileToRead.txt', 'r');
+        file.createReadStream().pipe(process.stdout);
     } catch (err) {
         console.error(err);
     }
