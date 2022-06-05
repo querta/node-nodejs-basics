7b5820490435d6331c5145d69dfcfb1757bd708b
qurta <afy@dr.com>
2022-06-05 02:12:30 +0200

    init: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs/promises';
+
+const checkExist = async (path) => {
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
 export const remove = async () => {
-    // Write your code here 
+    try {
+        if (!(await checkExist('./files/fileToRemove.txt')))
+            console.log(new Error("FS operation failed"));    
+        else 
+            await fs.unlink('./files/fileToRemove.txt');
+    } catch(err) {
+        console.error(err);        
+    }
 };
+remove();
