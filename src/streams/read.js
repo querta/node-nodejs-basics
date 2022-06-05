d8552ee36f6015988a1776f65f9ea2827937712d
qurta <afy@dr.com>
2022-06-05 13:23:04 +0200

    refactor: delete comments in streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,7 +2,6 @@ import { open } from 'fs/promises';
 
 export const read = async () => {
     try {
-        // const stream = await createReadStream('./files/fileToRead.txt', 'utf8');
         const file = await open('./files/fileToRead.txt', 'r');
         file.createReadStream().pipe(process.stdout);
     } catch (err) {
